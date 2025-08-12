03647016c479746edcc8ddea942f52a176d7309a
Bruno Nunes <166269531+brunonunesx@users.noreply.github.com>
2025-08-11 23:11:29 -0300

    Add files via upload

---
+++ modulo.3/liguagem de programaçao/supresa/script.js
@@ -0,0 +1,4 @@
+function mudarMensagem (){
+    const mensagem = document.getElementById ("mensagem"); 
+    mensagem.innerHTML = "Você acabou de ganhar 5 % de desconto em todos os produtos do site ";
+}
